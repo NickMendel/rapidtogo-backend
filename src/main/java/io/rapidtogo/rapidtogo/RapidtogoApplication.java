6d3b4a1d8983e05d060f2ac0e989020d0ab5a8c2
NickMendel <nick.mendel@web.de>
2024-11-30 17:35:11 +0100

    init spring boot project

---
+++ src/main/java/io/rapidtogo/rapidtogo/RapidtogoApplication.java
@@ -0,0 +1,13 @@
+package io.rapidtogo.rapidtogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidtogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidtogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/rapidtogo/rapidtogo/RapidtogoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rapidtogo.rapidtogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidtogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
