be9f0896f4626616845397c67fa38e3b0db60f40
NickMendel <nick.mendel@web.de>
2024-11-30 18:36:14 +0100

    create Review Entity

---
+++ src/main/java/io/rapidtogo/rapidtogo/review/model/Review.java
@@ -0,0 +1,42 @@
+package io.rapidtogo.rapidtogo.review.model;
+
+import io.rapidtogo.rapidtogo.restaurant.model.Restaurant;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "review")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+
+  @Column(name = "created_at", nullable = false)
+  private final LocalDateTime createdAt = LocalDateTime.now(ZoneOffset.UTC);
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "review_id_seq")
+  @SequenceGenerator(name = "review_id_seq", sequenceName = "review_id_seq", allocationSize = 1)
+  private Long id;
+  @Column(name = "content")
+  private String content;
+  @Column(name = "quality_score", nullable = false)
+  private int qualityScore;
+  @Column(name = "delivery_score")
+  private int deliveryScore;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Restaurant restaurant;
+}
