5640073c98b230b4250651457ceb47376b3a2f14
NickMendel <nick.mendel@web.de>
2024-12-01 11:30:21 +0100

    create UniqueNameException

---
+++ src/main/java/io/rapidtogo/rapidtogo/exception/UniqueNameException.java
@@ -0,0 +1,8 @@
+package io.rapidtogo.rapidtogo.exception;
+
+public class UniqueNameException extends RuntimeException {
+
+  public UniqueNameException(String message) {
+    super(message);
+  }
+}
