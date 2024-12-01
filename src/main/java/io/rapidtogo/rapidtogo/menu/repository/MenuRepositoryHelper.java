16745404dc35c61d971a566493ce4e2a3360acb3
NickMendel <nick.mendel@web.de>
2024-12-01 11:24:49 +0100

    add findById() and findByRestaurantIdAndMenuId() in MenuRepositoryHelper, add Javadocs

--- src/main/java/io/rapidtogo/rapidtogo/menu/repository/MenuRepositoryHelper.java
+++ src/main/java/io/rapidtogo/rapidtogo/menu/repository/MenuRepositoryHelper.java
@@ -12,7 +12,14 @@ public class MenuRepositoryHelper {
 
   private final MenuRepository menuRepository;
 
-  public List<Menu> getAllByRestaurantId(Long restaurantId) {
+  /**
+   * Get all menus by restaurant ID
+   *
+   * @param restaurantId ID of the restaurant
+   * @return List of all menus
+   * @throws EntityNotFoundException if no menus are found
+   */
+  public List<Menu> findAllByRestaurantId(Long restaurantId) {
 
     List<Menu> menus = menuRepository.findAllByRestaurantId(restaurantId);
 
@@ -21,5 +28,39 @@ public class MenuRepositoryHelper {
     }
 
     return menus;
+  }
+
+  /**
+   * Get menu by restaurant ID and menu ID
+   *
+   * @param restaurantId ID of the restaurant
+   * @param menuId       ID of the menu
+   * @return Menu with the given ID
+   * @throws EntityNotFoundException if no menu is found with the given ID
+   */
+  public Menu findByRestaurantIdAndMenuId(Long restaurantId, Long menuId) {
+
+    return menuRepository.findByRestaurantIdAndId(restaurantId, menuId).orElseThrow(
+        () -> new EntityNotFoundException(
+            "Menu with ID: " + menuId + " not found for restaurant ID: " + restaurantId));
+  }
+
+  /**
+   * Get menu by ID
+   *
+   * @param menuId ID of the menu
+   * @return Menu with the given ID
+   * @throws EntityNotFoundException if no menu is found with the given ID
+   */
+  public Menu findById(Long menuId) {
+
+    return menuRepository.findById(menuId).orElseThrow(
+        () -> new EntityNotFoundException("Menu with ID: " + menuId + " not found"));
+  }
+
+  public void checkExistence(Long menuId) {
+    if (!menuRepository.existsById(menuId)) {
+      throw new EntityNotFoundException("Menu with ID: " + menuId + " not found");
+    }
   }
 }
