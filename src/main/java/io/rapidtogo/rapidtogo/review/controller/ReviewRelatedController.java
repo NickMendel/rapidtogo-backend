c2ffaf0fd9413c9a891495bf0e95de6f521a460e
NickMendel <nick.mendel@web.de>
2024-12-01 13:05:58 +0100

    create ReviewCustomerController with POST, PUT and DELETE endpoints, add GET endpoint in ReviewRelatedController

--- src/main/java/io/rapidtogo/rapidtogo/review/controller/ReviewRelatedController.java
+++ src/main/java/io/rapidtogo/rapidtogo/review/controller/ReviewRelatedController.java
@@ -1,10 +1,18 @@
 package io.rapidtogo.rapidtogo.review.controller;
 
+import io.rapidtogo.rapidtogo.review.dto.ReviewResponse;
+import io.rapidtogo.rapidtogo.review.service.ReviewService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/v1")
@@ -13,4 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 public class ReviewRelatedController {
 
+  private final ReviewService reviewService;
+
+  @GetMapping("/restaurants/{restaurantId}/reviews")
+  public ResponseEntity<List<ReviewResponse>> getAllReviewsByRestaurantId(
+      @PathVariable Long restaurantId) {
+
+    List<ReviewResponse> reviews = reviewService.getAllReviewsByRestaurantId(restaurantId);
+    log.info("Found {} reviews for restaurant with id: {}", reviews.size(), restaurantId);
+
+    return ResponseEntity.status(HttpStatus.OK).body(reviews);
+  }
 }
