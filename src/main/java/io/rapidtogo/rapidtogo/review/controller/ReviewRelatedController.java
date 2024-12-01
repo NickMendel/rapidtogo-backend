d483d3e763094d2808cf864ba1f2c1d94fbd795b
Nick Mendel <85950553+nickmendel@users.noreply.github.com>
2024-12-01 12:09:50 +0100

    create ReviewRelatedController

---
+++ src/main/java/io/rapidtogo/rapidtogo/review/controller/ReviewRelatedController.java
@@ -0,0 +1,16 @@
+package io.rapidtogo.rapidtogo.review.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+@RequiredArgsConstructor
+@Slf4j
+@CrossOrigin(origins = "*")
+public class ReviewRelatedController {
+
+}
