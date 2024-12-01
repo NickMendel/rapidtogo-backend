3c77d25a2121950a960ff825f8b7fa780f22e3f8
NickMendel <nick.mendel@web.de>
2024-12-01 11:29:21 +0100

    add mapToEntity() and updateEntity() in ProductMapper

--- src/main/java/io/rapidtogo/rapidtogo/product/mapper/ProductMapper.java
+++ src/main/java/io/rapidtogo/rapidtogo/product/mapper/ProductMapper.java
@@ -1,13 +1,35 @@
 package io.rapidtogo.rapidtogo.product.mapper;
 
 import io.rapidtogo.rapidtogo.product.dto.ProductMinimalResponse;
+import io.rapidtogo.rapidtogo.product.dto.ProductRequest;
+import io.rapidtogo.rapidtogo.product.enums.Allergy;
 import io.rapidtogo.rapidtogo.product.model.Product;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductMapper {
+
+  public Product mapToEntity(ProductRequest request) {
+
+    if (request == null) {
+      return null;
+    }
+
+    Product product = new Product();
+    product.setName(request.getName());
+    product.setDescription(request.getDescription());
+    product.setPrice(request.getPrice());
+
+    if (request.getAllergies() != null && !request.getAllergies().isEmpty()) {
+      product.setAllergies(
+          request.getAllergies().stream().map(Allergy::valueOf).collect(Collectors.toSet()));
+    }
+
+    return product;
+  }
 
   public ProductMinimalResponse mapToMinimalDto(Product product) {
 
@@ -30,5 +52,21 @@ public class ProductMapper {
     }
 
     return products.stream().map(this::mapToMinimalDto).toList();
+  }
+
+  public void updateEntity(Product product, ProductRequest request) {
+
+    if (product == null || request == null) {
+      return;
+    }
+
+    product.setName(request.getName());
+    product.setDescription(request.getDescription());
+    product.setPrice(request.getPrice());
+
+    if (request.getAllergies() != null && !request.getAllergies().isEmpty()) {
+      product.setAllergies(
+          request.getAllergies().stream().map(Allergy::valueOf).collect(Collectors.toSet()));
+    }
   }
 }
