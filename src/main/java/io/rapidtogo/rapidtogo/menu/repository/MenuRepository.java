a540b1ac6dd68fa04be03fe5a8de33f031648cf7
NickMendel <nick.mendel@web.de>
2024-12-01 11:24:09 +0100

    add findByRestaurantIdAndId() in MenuRepository

--- src/main/java/io/rapidtogo/rapidtogo/menu/repository/MenuRepository.java
+++ src/main/java/io/rapidtogo/rapidtogo/menu/repository/MenuRepository.java
@@ -2,6 +2,7 @@ package io.rapidtogo.rapidtogo.menu.repository;
 
 import io.rapidtogo.rapidtogo.menu.model.Menu;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,6 @@ public interface MenuRepository extends JpaRepository<Menu, Long> {
   @Query("SELECT m FROM Menu m WHERE m.restaurant.id = :restaurantId")
   List<Menu> findAllByRestaurantId(Long restaurantId);
 
+  @Query("SELECT m FROM Menu m WHERE m.restaurant.id = :restaurantId AND m.id = :menuId")
+  Optional<Menu> findByRestaurantIdAndId(Long restaurantId, Long menuId);
 }
