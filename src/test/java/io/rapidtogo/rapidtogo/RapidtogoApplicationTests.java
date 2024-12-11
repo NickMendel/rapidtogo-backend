3c27b8619f859bea91c7c1d9163a570aec3b2def
NickMendel <nick.mendel@web.de>
2024-12-11 17:07:45 +0100

    add ActiveProfile annotation to Test Class, remove test step

--- src/test/java/io/rapidtogo/rapidtogo/RapidtogoApplicationTests.java
+++ src/test/java/io/rapidtogo/rapidtogo/RapidtogoApplicationTests.java
@@ -2,7 +2,9 @@ package io.rapidtogo.rapidtogo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class RapidtogoApplicationTests {
 
