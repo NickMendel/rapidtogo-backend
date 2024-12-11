c05fff05d385f0941f07ce60e662e2b39ae51302
NickMendel <nick.mendel@web.de>
2024-12-11 18:17:10 +0100

    add AuthenticationPrincipal Annotation in MenuPartnerController, add new endpoint getMenuByIdAndByRestaurantId() in MenuCustomerController

--- src/main/java/io/rapidtogo/rapidtogo/customer/menu/controller/MenuCustomerController.java
+++ src/main/java/io/rapidtogo/rapidtogo/customer/menu/controller/MenuCustomerController.java
@@ -1,12 +1,14 @@
 package io.rapidtogo.rapidtogo.customer.menu.controller;
 
+import io.rapidtogo.rapidtogo.customer.menu.service.MenuCustomerService;
 import io.rapidtogo.rapidtogo.partner.menu.dto.MenuResponse;
-import io.rapidtogo.rapidtogo.partner.menu.service.MenuService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,15 +22,26 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class MenuCustomerController {
 
-  private final MenuService menuService;
+  private final MenuCustomerService menuCustomerService;
 
   @GetMapping("/restaurants/{restaurantId}/menus")
   public ResponseEntity<List<MenuResponse>> getAllMenusByRestaurantId(
-      @PathVariable Long restaurantId) {
+      @PathVariable Long restaurantId, @AuthenticationPrincipal Jwt jwt) {
 
-    List<MenuResponse> menus = menuService.getAllMenusByRestaurantId(restaurantId);
+    List<MenuResponse> menus = menuCustomerService.getAllMenusByRestaurantId(restaurantId);
     log.info("Menus for restaurant ID {} fetched successfully", restaurantId);
 
     return ResponseEntity.status(HttpStatus.OK).body(menus);
+  }
+
+  @GetMapping("/restaurants/{restaurantId}/menus/{menuId}")
+  public ResponseEntity<MenuResponse> getMenuByIdAndByRestaurantId(
+      @PathVariable Long restaurantId, @PathVariable Long menuId,
+      @AuthenticationPrincipal Jwt jwt) {
+
+    MenuResponse menu = menuCustomerService.getMenuByIdAndByRestaurantId(menuId, restaurantId);
+    log.info("Menu with ID {} for restaurant ID {} fetched successfully", menuId, restaurantId);
+
+    return ResponseEntity.status(HttpStatus.OK).body(menu);
   }
 }
--- src/main/java/io/rapidtogo/rapidtogo/partner/menu/controller/MenuPartnerController.java
+++ src/main/java/io/rapidtogo/rapidtogo/partner/menu/controller/MenuPartnerController.java
@@ -2,13 +2,15 @@ package io.rapidtogo.rapidtogo.partner.menu.controller;
 
 import io.rapidtogo.rapidtogo.partner.menu.dto.MenuRequest;
 import io.rapidtogo.rapidtogo.partner.menu.dto.MenuResponse;
-import io.rapidtogo.rapidtogo.partner.menu.service.MenuService;
+import io.rapidtogo.rapidtogo.partner.menu.service.MenuPartnerService;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,19 +22,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/partner")
+@RequestMapping("/v1/partners")
 @RequiredArgsConstructor
 @Slf4j
 @CrossOrigin(origins = "*")
 public class MenuPartnerController {
 
-  private final MenuService menuService;
+  private final MenuPartnerService menuPartnerService;
 
   @PostMapping("/restaurants/{restaurantId}/menus")
   public ResponseEntity<String> createMenu(@PathVariable Long restaurantId,
-      @RequestBody @Valid MenuRequest request) {
+      @RequestBody @Valid MenuRequest request, @AuthenticationPrincipal Jwt jwt) {
 
-    String successMessage = menuService.createMenu(restaurantId, request);
+    String successMessage = menuPartnerService.createMenu(restaurantId, request, jwt);
     log.info("Menu for restaurant ID {} created successfully", restaurantId);
 
     return ResponseEntity.status(HttpStatus.CREATED).body(successMessage);
@@ -40,9 +42,9 @@ public class MenuPartnerController {
 
   @GetMapping("/restaurants/{restaurantId}/menus")
   public ResponseEntity<List<MenuResponse>> getAllMenusByRestaurantId(
-      @PathVariable Long restaurantId) {
+      @PathVariable Long restaurantId, @AuthenticationPrincipal Jwt jwt) {
 
-    List<MenuResponse> menus = menuService.getAllMenusByRestaurantId(restaurantId);
+    List<MenuResponse> menus = menuPartnerService.getAllMenusByRestaurantId(restaurantId, jwt);
     log.info("Menus for restaurant ID {} fetched successfully", restaurantId);
 
     return ResponseEntity.status(HttpStatus.OK).body(menus);
@@ -50,9 +52,10 @@ public class MenuPartnerController {
 
   @GetMapping("/restaurants/{restaurantId}/menus/{menuId}")
   public ResponseEntity<MenuResponse> getMenuByIdAndByRestaurantId(
-      @PathVariable Long restaurantId, @PathVariable Long menuId) {
+      @PathVariable Long restaurantId, @PathVariable Long menuId,
+      @AuthenticationPrincipal Jwt jwt) {
 
-    MenuResponse menu = menuService.getMenuByIdAndByRestaurantId(menuId, restaurantId);
+    MenuResponse menu = menuPartnerService.getMenuByIdAndByRestaurantId(menuId, restaurantId, jwt);
     log.info("Menu with ID {} for restaurant ID {} fetched successfully", menuId, restaurantId);
 
     return ResponseEntity.status(HttpStatus.OK).body(menu);
@@ -60,9 +63,10 @@ public class MenuPartnerController {
 
   @PatchMapping("/restaurants/{restaurantId}/menus/{menuId}")
   public ResponseEntity<String> updateMenu(@PathVariable Long restaurantId,
-      @PathVariable Long menuId, @RequestBody @Valid MenuRequest request) {
+      @PathVariable Long menuId, @RequestBody @Valid MenuRequest request,
+      @AuthenticationPrincipal Jwt jwt) {
 
-    String successMessage = menuService.updateMenu(restaurantId, menuId, request);
+    String successMessage = menuPartnerService.updateMenu(restaurantId, menuId, request, jwt);
     log.info("Menu with ID {} for restaurant ID {} updated successfully", menuId, restaurantId);
 
     return ResponseEntity.status(HttpStatus.OK).body(successMessage);
@@ -70,9 +74,9 @@ public class MenuPartnerController {
 
   @DeleteMapping("/restaurants/{restaurantId}/menus/{menuId}")
   public ResponseEntity<String> deleteMenu(@PathVariable Long restaurantId,
-      @PathVariable Long menuId) {
+      @PathVariable Long menuId, @AuthenticationPrincipal Jwt jwt) {
 
-    String successMessage = menuService.deleteMenu(restaurantId, menuId);
+    String successMessage = menuPartnerService.deleteMenu(restaurantId, menuId, jwt);
     log.info("Menu with ID {} for restaurant ID {} deleted successfully", menuId, restaurantId);
 
     return ResponseEntity.status(HttpStatus.NO_CONTENT).body(successMessage);
