d4d7e15d392d412d4d78751f05f0a0e1e32bff14
NickMendel <nick.mendel@web.de>
2024-12-11 18:16:00 +0100

    add new methods and queries using userId in MenuRepository and Helper class

--- src/main/java/io/rapidtogo/rapidtogo/partner/menu/repository/MenuRepository.java
+++ src/main/java/io/rapidtogo/rapidtogo/partner/menu/repository/MenuRepository.java
@@ -15,6 +15,18 @@ public interface MenuRepository extends JpaRepository<Menu, Long> {
   @Query("SELECT m FROM Menu m WHERE m.restaurant.id = :restaurantId")
   List<Menu> findAllByRestaurantId(Long restaurantId);
 
+  @Query("SELECT m FROM Menu m WHERE m.restaurant.id = :restaurantId AND m.userId = :userId")
+  List<Menu> findAllByRestaurantId(Long restaurantId, String userId);
+
+  @Query("SELECT m FROM Menu m WHERE m.restaurant.id = :restaurantId AND m.id = :menuId AND m.userId = :userId")
+  Optional<Menu> findByRestaurantIdAndById(Long restaurantId, Long menuId, String userId);
+
   @Query("SELECT m FROM Menu m WHERE m.restaurant.id = :restaurantId AND m.id = :menuId")
   Optional<Menu> findByRestaurantIdAndById(Long restaurantId, Long menuId);
+
+  @Query("SELECT m FROM Menu m WHERE m.id = :menuId AND m.userId = :userId")
+  Optional<Menu> findById(Long menuId, String userId);
+
+  @Query("SELECT CASE WHEN COUNT(m) > 0 THEN TRUE ELSE FALSE END FROM Menu m WHERE m.id = :menuId AND m.userId = :userId")
+  boolean existsById(Long menuId, String userId);
 }
--- src/main/java/io/rapidtogo/rapidtogo/partner/menu/repository/MenuRepositoryHelper.java
+++ src/main/java/io/rapidtogo/rapidtogo/partner/menu/repository/MenuRepositoryHelper.java
@@ -13,6 +13,25 @@ public class MenuRepositoryHelper {
   private final MenuRepository menuRepository;
 
   /**
+   * Get all menus by restaurant ID owned by a user
+   *
+   * @param restaurantId ID of the restaurant
+   * @param userId       ID of the user who owns the restaurant
+   * @return List of all menus
+   * @throws EntityNotFoundException if no menus are found
+   */
+  public List<Menu> findAllByRestaurantId(Long restaurantId, String userId) {
+
+    List<Menu> menus = menuRepository.findAllByRestaurantId(restaurantId, userId);
+
+    if (menus.isEmpty()) {
+      throw new EntityNotFoundException("No menus found for restaurant ID " + restaurantId);
+    }
+
+    return menus;
+  }
+
+  /**
    * Get all menus by restaurant ID
    *
    * @param restaurantId ID of the restaurant
@@ -46,6 +65,21 @@ public class MenuRepositoryHelper {
   }
 
   /**
+   * Get menu by restaurant ID and menu ID
+   *
+   * @param restaurantId ID of the restaurant
+   * @param menuId       ID of the menu
+   * @return Menu with the given ID
+   * @throws EntityNotFoundException if no menu is found with the given ID
+   */
+  public Menu findByRestaurantIdAndById(Long restaurantId, Long menuId, String userId) {
+
+    return menuRepository.findByRestaurantIdAndById(restaurantId, menuId, userId).orElseThrow(
+        () -> new EntityNotFoundException(
+            "Menu with ID: " + menuId + " not found for restaurant ID: " + restaurantId));
+  }
+
+  /**
    * Get menu by ID
    *
    * @param menuId ID of the menu
@@ -58,8 +92,43 @@ public class MenuRepositoryHelper {
         () -> new EntityNotFoundException("Menu with ID: " + menuId + " not found"));
   }
 
+  /**
+   * Get menu by ID owned by a user
+   *
+   * @param menuId ID of the menu
+   * @param userId ID of the user who owns the menu
+   * @return Menu with the given ID
+   * @throws EntityNotFoundException if no menu is found with the given ID
+   */
+  public Menu findById(Long menuId, String userId) {
+
+    return menuRepository.findById(menuId, userId).orElseThrow(
+        () -> new EntityNotFoundException("Menu with ID: " + menuId + " not found"));
+  }
+
+  /**
+   * Check if menu exists
+   *
+   * @param menuId ID of the menu
+   * @throws EntityNotFoundException if no menu is found with the given ID
+   */
   public void checkExistence(Long menuId) {
+
     if (!menuRepository.existsById(menuId)) {
+      throw new EntityNotFoundException("Menu with ID: " + menuId + " not found");
+    }
+  }
+
+  /**
+   * Check if menu exists owned by a user
+   *
+   * @param menuId ID of the menu
+   * @param userId ID of the user who owns the menu
+   * @throws EntityNotFoundException if no menu is found with the given ID
+   */
+  public void checkExistence(Long menuId, String userId) {
+
+    if (!menuRepository.existsById(menuId, userId)) {
       throw new EntityNotFoundException("Menu with ID: " + menuId + " not found");
     }
   }
