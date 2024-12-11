b7fffdcac8fa1a3d381a2ef384b0be10bb173758
NickMendel <nick.mendel@web.de>
2024-12-11 18:14:05 +0100

    add AuthenticationPrincipal in RestaurantPartnerController and jwt/userId parameter to methods in RestaurantPartnerService

--- src/main/java/io/rapidtogo/rapidtogo/partner/restaurant/controller/RestaurantPartnerController.java
+++ src/main/java/io/rapidtogo/rapidtogo/partner/restaurant/controller/RestaurantPartnerController.java
@@ -11,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,28 +25,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/partner/restaurants")
+@RequestMapping("/v1/partners/restaurants")
 @RequiredArgsConstructor
 @Slf4j
-@CrossOrigin(origins = "*")
+@CrossOrigin("*")
 public class RestaurantPartnerController {
 
   private final RestaurantPartnerService restaurantPartnerService;
 
   @PostMapping
   public ResponseEntity<String> createRestaurant(
-      @RequestBody @Valid RestaurantCreateRequest request) {
+      @RequestBody @Valid RestaurantCreateRequest request, @AuthenticationPrincipal Jwt jwt) {
 
-    String successMessage = restaurantPartnerService.createRestaurant(request);
+    String successMessage = restaurantPartnerService.createRestaurant(request, jwt);
     log.info("Restaurant with name {} created successfully.", request.getName());
 
     return ResponseEntity.status(HttpStatus.CREATED).body(successMessage);
   }
 
   @GetMapping
-  public ResponseEntity<List<RestaurantMinimalResponse>> getAllRestaurants() {
+  public ResponseEntity<List<RestaurantMinimalResponse>> getAllRestaurants(
+      @AuthenticationPrincipal Jwt jwt) {
 
-    List<RestaurantMinimalResponse> restaurants = restaurantPartnerService.getAllRestaurants();
+    List<RestaurantMinimalResponse> restaurants = restaurantPartnerService.getAllRestaurants(jwt);
     log.info("Retrieved {} restaurants.", restaurants.size());
 
     return ResponseEntity.status(HttpStatus.OK).body(restaurants);
@@ -52,10 +55,10 @@ public class RestaurantPartnerController {
 
   @GetMapping("/{restaurantId}")
   public ResponseEntity<RestaurantDetailedResponse> getRestaurantById(
-      @PathVariable Long restaurantId) {
+      @PathVariable Long restaurantId, @AuthenticationPrincipal Jwt jwt) {
 
-    RestaurantDetailedResponse restaurant = restaurantPartnerService.getRestaurantById(
-        restaurantId);
+    RestaurantDetailedResponse restaurant = restaurantPartnerService.getRestaurantById(restaurantId,
+        jwt);
     log.info("Retrieved restaurant with id {}.", restaurantId);
 
     return ResponseEntity.status(HttpStatus.OK).body(restaurant);
@@ -63,10 +66,10 @@ public class RestaurantPartnerController {
 
   @GetMapping(params = "location")
   public ResponseEntity<List<RestaurantMinimalResponse>> getAllRestaurantsByLocation(
-      @RequestParam String location) {
+      @RequestParam String location, @AuthenticationPrincipal Jwt jwt) {
 
     List<RestaurantMinimalResponse> restaurants = restaurantPartnerService.getAllRestaurantsByCity(
-        location);
+        location, jwt);
     log.info("Retrieved {} restaurants in city {}.", restaurants.size(), location);
 
     return ResponseEntity.status(HttpStatus.OK).body(restaurants);
@@ -74,18 +77,20 @@ public class RestaurantPartnerController {
 
   @PutMapping("/{restaurantId}")
   public ResponseEntity<String> updateRestaurant(@PathVariable Long restaurantId,
-      @RequestBody @Valid RestaurantUpdateRequest request) {
+      @RequestBody @Valid RestaurantUpdateRequest request, @AuthenticationPrincipal Jwt jwt) {
 
-    String successMessage = restaurantPartnerService.updateRestaurantById(restaurantId, request);
+    String successMessage = restaurantPartnerService.updateRestaurantById(restaurantId, request,
+        jwt);
     log.info("Restaurant with id {} updated successfully.", restaurantId);
 
     return ResponseEntity.status(HttpStatus.OK).body(successMessage);
   }
 
   @DeleteMapping("/{restaurantId}")
-  public ResponseEntity<String> deleteRestaurant(@PathVariable Long restaurantId) {
+  public ResponseEntity<String> deleteRestaurant(@PathVariable Long restaurantId,
+      @AuthenticationPrincipal Jwt jwt) {
 
-    String successMessage = restaurantPartnerService.deleteRestaurantById(restaurantId);
+    String successMessage = restaurantPartnerService.deleteRestaurantById(restaurantId, jwt);
     log.info("Restaurant with id {} deleted successfully.", restaurantId);
 
     return ResponseEntity.status(HttpStatus.NO_CONTENT).body(successMessage);
--- src/main/java/io/rapidtogo/rapidtogo/partner/restaurant/service/RestaurantPartnerService.java
+++ src/main/java/io/rapidtogo/rapidtogo/partner/restaurant/service/RestaurantPartnerService.java
@@ -10,6 +10,7 @@ import io.rapidtogo.rapidtogo.partner.restaurant.repository.RestaurantRepository
 import io.rapidtogo.rapidtogo.partner.restaurant.repository.RestaurantRepositoryHelper;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,37 +23,39 @@ public class RestaurantPartnerService {
   private final RestaurantRepositoryHelper restaurantRepositoryHelper;
 
   @Transactional
-  public String createRestaurant(RestaurantCreateRequest request) {
+  public String createRestaurant(RestaurantCreateRequest request, Jwt jwt) {
 
-    restaurantRepository.save(restaurantMapper.mapToEntity(request));
+    restaurantRepository.save(restaurantMapper.mapToEntity(request, jwt.getSubject()));
 
     return "Restaurant created successfully";
   }
 
   @Transactional(readOnly = true)
-  public List<RestaurantMinimalResponse> getAllRestaurants() {
+  public List<RestaurantMinimalResponse> getAllRestaurants(Jwt jwt) {
 
-    return restaurantMapper.mapToMinimalListDto(restaurantRepositoryHelper.findAll());
+    return restaurantMapper.mapToMinimalListDto(
+        restaurantRepositoryHelper.findAll(jwt.getSubject()));
   }
 
   @Transactional(readOnly = true)
-  public RestaurantDetailedResponse getRestaurantById(Long restaurantId) {
+  public RestaurantDetailedResponse getRestaurantById(Long restaurantId, Jwt jwt) {
 
-    Restaurant restaurant = restaurantRepositoryHelper.findById(restaurantId);
+    Restaurant restaurant = restaurantRepositoryHelper.findById(restaurantId, jwt.getSubject());
 
     return restaurantMapper.mapToDetailedDto(restaurant);
   }
 
   @Transactional(readOnly = true)
-  public List<RestaurantMinimalResponse> getAllRestaurantsByCity(String city) {
+  public List<RestaurantMinimalResponse> getAllRestaurantsByCity(String city, Jwt jwt) {
 
-    return restaurantMapper.mapToMinimalListDto(restaurantRepositoryHelper.findAllByCity(city));
+    return restaurantMapper.mapToMinimalListDto(
+        restaurantRepositoryHelper.findAllByCity(city, jwt.getSubject()));
   }
 
   @Transactional
-  public String updateRestaurantById(Long restaurantId, RestaurantUpdateRequest request) {
+  public String updateRestaurantById(Long restaurantId, RestaurantUpdateRequest request, Jwt jwt) {
 
-    Restaurant restaurant = restaurantRepositoryHelper.findById(restaurantId);
+    Restaurant restaurant = restaurantRepositoryHelper.findById(restaurantId, jwt.getSubject());
     restaurant = restaurantMapper.updateEntity(restaurant, request);
     restaurantRepository.save(restaurant);
 
@@ -60,9 +63,9 @@ public class RestaurantPartnerService {
   }
 
   @Transactional
-  public String deleteRestaurantById(Long restaurantId) {
+  public String deleteRestaurantById(Long restaurantId, Jwt jwt) {
 
-    restaurantRepositoryHelper.deleteById(restaurantId);
+    restaurantRepositoryHelper.deleteById(restaurantId, jwt.getSubject());
 
     return "Restaurant deleted successfully";
   }
